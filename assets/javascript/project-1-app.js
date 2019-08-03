cd2e2b7af70855a1db0763445b1965924202f15f
Danny Kim <dudkny@gmail.com>
2019-08-03 06:20:56 -0500

    Added basic files and comments describing general process flow

---
+++ assets/javascript/project-1-app.js
@@ -0,0 +1,38 @@
+// GLOBAL VARIABLES //
+// Declare global variables.
+
+
+// DOM //
+// Declare connections with DOM.
+
+
+// API CONFIGURATIONS //
+// Configure connection to API endpoints.
+
+
+// DATA ENTRY EVENT HANDLERS //
+// Add data entry functions with validation.
+// Take user input of preferred city.
+// Take user input of preferred date/range.
+// Take user input of preferred budget.
+
+
+// SKYSCANNER API //
+// Make AJAX call to API to import relevant flight data.
+
+
+// WEATHER API //
+// Make AJAX call to API to import weather data.
+
+
+// DATA PROCESSING FUNCTION //
+// Filter, evaluate and reorganize data before rendering.
+
+
+// SUGGEST & RENDER FUNCTION //
+// Take reorganized data and output to display.
+
+
+// STORAGE FUNCTION //
+// Allow saving/tracking of multiple query data.
+// Optionally get/set some query data in database for persistence/tracking.
