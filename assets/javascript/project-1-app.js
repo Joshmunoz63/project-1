7dc466d2887d637d17f9019d1e65c44c35d4f2f4
Danny Kim <dudkny@gmail.com>
2019-08-03 06:28:44 -0500

    Added basic files and comments describing general process flow

--- assets/javascript/project-1-app.js
+++ assets/javascript/project-1-app.js
@@ -15,14 +15,15 @@
 // Take user input of preferred city.
 // Take user input of preferred date/range.
 // Take user input of preferred budget.
+// Optional: Consider taking info such as intended activity/purpose.
 
 
 // SKYSCANNER API //
-// Make AJAX call to API to import relevant flight data.
+// Make AJAX call to API to import relevant flight data. (airlines, fares, dates, etc.)
 
 
 // WEATHER API //
-// Make AJAX call to API to import weather data.
+// Make AJAX call to API to import weather data. (chance of rain, wind, humidity, etc.)
 
 
 // DATA PROCESSING FUNCTION //
@@ -31,8 +32,9 @@
 
 // SUGGEST & RENDER FUNCTION //
 // Take reorganized data and output to display.
+// Optional: In addition to flight dates & lines, suggest things to do or places to visit at destination. Maybe hotel suggestions.
 
 
 // STORAGE FUNCTION //
 // Allow saving/tracking of multiple query data.
-// Optionally get/set some query data in database for persistence/tracking.
+// Optional: Get/set some query data in database for persistence/tracking.
