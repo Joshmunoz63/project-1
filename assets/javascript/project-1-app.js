12b67dae3825f8f528c1e001c062abf2f0dc4498
Jose Marin Hernandez <jmhern25@yahoo.com>
2019-08-04 14:24:54 -0500

    Started working on the openWeatherAPI calls. Have something working that displays the 5 day/ 3 hour forcast that displays the max temp, min temp, and conditions.

--- assets/javascript/project-1-app.js
+++ assets/javascript/project-1-app.js
@@ -1,7 +1,9 @@
+$(document).ready(function () {
 // GLOBAL VARIABLES //
 // Declare global variables.
 
-
+var cityName = "";
+var openWeatherQueryURL = "";
 // DOM //
 // Declare connections with DOM.
 
@@ -26,6 +28,29 @@
 // Make AJAX call to API to import weather data. (chance of rain, wind, humidity, etc.)
 
 
+//Functions -----------------------------------------
+function callOpenWeatherAPI() {
+    cityName = "Houston"
+    openWeatherQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName +"&units=imperial&APPID=eaea7d39c63b0abce29025a25d630226";
+    console.log(openWeatherQueryURL);
+    $.ajax({
+        url: openWeatherQueryURL,
+        method: "GET",
+        crossDomain: true,
+    }).then(function (openWeatherAPICall) {
+        console.log(openWeatherAPICall);
+        console.log("Here is the weather forcast for: " + openWeatherAPICall.city.name); 
+        for(var i = 0; i < openWeatherAPICall.list.length; i++) {
+            console.log(" ");
+            console.log(openWeatherAPICall.list[i].dt_txt);
+            console.log("Temp: " + openWeatherAPICall.list[i].main.temp);
+            console.log("Max Temp: " + openWeatherAPICall.list[i].main.temp_max);
+            console.log("Min Temp: " + openWeatherAPICall.list[i].main.temp_min);
+            console.log("Conditions: " + openWeatherAPICall.list[i].weather[0].description);
+            console.log(" ");
+        }
+    });
+}
 // DATA PROCESSING FUNCTION //
 // Filter, evaluate and reorganize data before rendering.
 
@@ -38,3 +63,12 @@
 // STORAGE FUNCTION //
 // Allow saving/tracking of multiple query data.
 // Optional: Get/set some query data in database for persistence/tracking.
+
+//End of Functions -----------------------------------------
+
+//Executable Code
+callOpenWeatherAPI();
+//End of executable code
+    
+});
+
