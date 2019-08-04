e098e74f70d9c41941bfbb6e8231b3998e954685
Jose Marin Hernandez <jmhern25@yahoo.com>
2019-08-04 15:46:35 -0500

    Got the two APIs communicating between one another. In the open weather function there coordinates of the city you are searching for will be saved and then passed to the TrailAPI function and then search for trails in the area. Now we will want to see what information we will display, how many days in advanced we want to display to the user, how many trails we want to display, and how we want to display this information.

--- assets/javascript/project-1-app.js
+++ assets/javascript/project-1-app.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
 
 var cityName = "";
 var openWeatherQueryURL = "";
+var trailAPIQueryURL = "";
+var lat = 0;
+var lon = 0;
 // DOM //
 // Declare connections with DOM.
 
@@ -30,27 +33,52 @@ var openWeatherQueryURL = "";
 
 //Functions -----------------------------------------
 function callOpenWeatherAPI() {
-    cityName = "Houston"
+    cityName = "Phoenix"
     openWeatherQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName +"&units=imperial&APPID=eaea7d39c63b0abce29025a25d630226";
-    console.log(openWeatherQueryURL);
     $.ajax({
         url: openWeatherQueryURL,
         method: "GET",
         crossDomain: true,
     }).then(function (openWeatherAPICall) {
+        lat = openWeatherAPICall.city.coord.lat;
+        lon = openWeatherAPICall.city.coord.lon;
+        queryTrail(lat, lon);
         console.log(openWeatherAPICall);
         console.log("Here is the weather forcast for: " + openWeatherAPICall.city.name); 
+        console.log("Lat: " + lat);
+        console.log("Lon: " + lon);
+        
+        
         for(var i = 0; i < openWeatherAPICall.list.length; i++) {
             console.log(" ");
             console.log(openWeatherAPICall.list[i].dt_txt);
             console.log("Temp: " + openWeatherAPICall.list[i].main.temp);
             console.log("Max Temp: " + openWeatherAPICall.list[i].main.temp_max);
             console.log("Min Temp: " + openWeatherAPICall.list[i].main.temp_min);
-            console.log("Conditions: " + openWeatherAPICall.list[i].weather[0].description);
+            console.log("Conditions: " + openWeatherAPICall.list[i].weather[0].main);
+            console.log("Ground Level: " + openWeatherAPICall.list[i].main.grnd_level);
+            console.log("Wind: " + openWeatherAPICall.list[i].wind.deg + " degrees");
+            console.log("Wind Speed: " + openWeatherAPICall.list[i].wind.speed);
             console.log(" ");
         }
     });
 }
+
+function queryTrail(lat, lon) {
+    trailAPIQueryURL = "https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=" + lat +"&lon=" + lon;
+    $.ajax({
+        url: trailAPIQueryURL,
+        method: 'GET',
+        headers: {
+            "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
+            "x-rapidapi-key": "cfbae3bd13msh660a849870aa5cap194a7fjsnd973bfb99523"
+        }
+    }).then(function (response) {
+        console.log(response);
+    }) // End of AJAX call to Trail API.
+} // End of function.
+
+// Make AJAX call to Trail API on load.
 // DATA PROCESSING FUNCTION //
 // Filter, evaluate and reorganize data before rendering.
 
@@ -67,6 +95,7 @@ function callOpenWeatherAPI() {
 //End of Functions -----------------------------------------
 
 //Executable Code
+
 callOpenWeatherAPI();
 //End of executable code
     
